8bceaab127b88945f88342ea23e03bb9ceb16e14
MistoMind <khushseervi@gmail.com>
2024-02-26 15:33:05 +0530

    Added pydantic settings support

--- app/constants.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-POSTGRES_URL = os.getenv("POSTGRES_URL")
--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-from app.constants import POSTGRES_URL
+from app.config import settings
 
 
 class Base(DeclarativeBase):
     pass
 
 
-engine = create_engine(POSTGRES_URL)
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
